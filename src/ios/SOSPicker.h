7a5a13597c9b51f22377a8f5f3d5267bfc8110f1
Antony Alkmim <antony-alkmim@hotmail.com>
2017-09-17 18:05:16 -0300

    change from ELCImagePicker to GMImagePicker

--- src/ios/SOSPicker.h
+++ src/ios/SOSPicker.h
@@ -7,18 +7,21 @@
 //
 
 #import <Cordova/CDVPlugin.h>
-#import "ELCAlbumPickerController.h"
-#import "ELCImagePickerController.h"
 
-@interface SOSPicker : CDVPlugin <ELCImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate>
+
+@interface SOSPicker : CDVPlugin < UINavigationControllerDelegate, UIScrollViewDelegate>
 
 @property (copy)   NSString* callbackId;
 
 - (void) getPictures:(CDVInvokedUrlCommand *)command;
+- (void) hasReadPermission:(CDVInvokedUrlCommand *)command;
+- (void) requestReadPermission:(CDVInvokedUrlCommand *)command;
+
 - (UIImage*)imageByScalingNotCroppingForSize:(UIImage*)anImage toSize:(CGSize)frameSize;
 
 @property (nonatomic, assign) NSInteger width;
 @property (nonatomic, assign) NSInteger height;
 @property (nonatomic, assign) NSInteger quality;
+@property (nonatomic, assign) NSInteger outputType;
 
 @end
